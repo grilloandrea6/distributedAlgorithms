4b145f2b18f69afa694d92d320228fc90c4d9daf
Andrea Grillo <andrea.grillo@epfl.ch>
2024-10-11 16:40:49 +0200

    outputLogger

--- src/main/java/cs451/OutputLogger.java
+++ src/main/java/cs451/OutputLogger.java
@@ -1,9 +1,42 @@
 package cs451;
 
-public class OutputLogger {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-    public OutputLogger(String output) {
-        //TODO Auto-generated constructor stub
+public class OutputLogger {
+    private static BufferedWriter writer;
+
+    public static void begin(Parser p) throws IOException {
+        writer = new BufferedWriter(new FileWriter(p.output()), 32768);
     }
 
+    public static void logDeliver(int senderId, byte[] data) {
+        synchronized (writer) {
+            try {
+                writer.write("d " + senderId + " " + NetworkInterface.bytesToInt(data));
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void logBroadcast(byte[] data) {
+        synchronized (writer) {
+            try {
+                writer.write("b " + NetworkInterface.bytesToInt(data));
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void end() throws IOException {
+        synchronized (writer) {
+            System.out.println("OutputLogger closing file");
+            writer.close();
+        }
+    }
 }
