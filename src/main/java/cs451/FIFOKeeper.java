33f2613e1f92f113466c02ec5ba47ac6b9284af4
Andrea Grillo <andrea.grillo@epfl.ch>
2024-12-21 23:35:48 +0100

    Update FIFOKeeper.java

--- src/main/java/cs451/FIFOKeeper.java
+++ src/main/java/cs451/FIFOKeeper.java
@@ -8,13 +8,9 @@ public class FIFOKeeper {
     static private int maximumInOrderMessage = 0;
     static private TreeMap<Integer,Set<Integer>> pending = new TreeMap<>();
 
-
     public static void addDecision(int n, Set<Integer> data) throws IOException {
-        // System.err.println("adding decision to shot " + n);
         if (n == maximumInOrderMessage + 1) {
-            // System.err.println("logging shot " + n);
             OutputLogger.logDecide(data);
-
             maximumInOrderMessage++;
             removeMessages();
         } else {
@@ -25,7 +21,6 @@ public class FIFOKeeper {
     private static void removeMessages() throws IOException {
         while (!pending.isEmpty() && pending.firstKey() == maximumInOrderMessage + 1) {
             maximumInOrderMessage++;
-            // System.err.println("logging shot " + maximumInOrderMessage);
             OutputLogger.logDecide(pending.pollFirstEntry().getValue());
         } 
     }
